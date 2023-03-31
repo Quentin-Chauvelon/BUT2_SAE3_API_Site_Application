11afec03d4f551e31632c02411b2897da5629769
E215723G <amedeo.calcagni@etu.univ-nantes.fr>
2023-03-31 19:02:19 +0200

    base_main_login

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +18,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val queue = Volley.newRequestQueue(this)
+        val loginURL = "http://172.26.82.56:443/schedules"
+
+        /* binding.inviter.setOnClickListener {
+             Intent(this,)
+         }*/
+        binding.seConnecter.setOnClickListener {
+
+// Request a string response from the provided URL.
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, loginURL, null,
+                { response ->
+                    binding.info.text = "Response: %s".format(response.toString())
+                },
+                { error ->
+                    binding.info.text = "error: %s".format(error.toString())
+                }
+            )
+
+// Add the request to the RequestQueue.
+            queue.add(jsonObjectRequest)
+            /*
+            if (binding.login.text.toString() == "test" && binding.motDePasse.text.toString() == "test"){
+                Intent(this,).PutExtra()
+            }*/
+        }
 
     }
 }
