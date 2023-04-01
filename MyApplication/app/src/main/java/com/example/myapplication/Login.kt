c2016f64e30ebc5108b2b9ddc2285d08d02d1fa5
E215723G <amedeo.calcagni@etu.univ-nantes.fr>
2023-04-01 13:02:06 +0200

    Tous les activity vide

--- MyApplication/app/src/main/java/com/example/myapplication/Login.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Login.kt
@@ -1,21 +1,17 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val queue = Volley.newRequestQueue(this)
@@ -25,8 +21,7 @@ class MainActivity : AppCompatActivity() {
              Intent(this,)
          }*/
         binding.seConnecter.setOnClickListener {
-
-// Request a string response from the provided URL.
+            // Request a string response from the provided URL.
             val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, loginURL, null,
                 { response ->
                     binding.info.text = "Response: %s".format(response.toString())
@@ -36,10 +31,10 @@ class MainActivity : AppCompatActivity() {
                 }
             )
 
-// Add the request to the RequestQueue.
+            // Add the request to the RequestQueue.
             queue.add(jsonObjectRequest)
             /*
-            if (binding.login.text.toString() == "test" && binding.motDePasse.text.toString() == "test"){
+            if (binding.Login.text.toString() == "test" && binding.motDePasse.text.toString() == "test"){
                 Intent(this,).PutExtra()
             }*/
         }
